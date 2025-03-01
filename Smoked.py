052a2907961c18dafd95eacc75f0e24c33be5159
Collin Chidiac <81669502+CollinChidiac@users.noreply.github.com>
2025-03-01 11:29:40 -0700

    Add files via upload

--- Smoked.py
+++ Smoked.py
@@ -17,13 +17,13 @@ def virtual_workout():
     exercise = random.choice(exercises)
     # Fixed rep count of 5
     reps = 5
-    print(f"Time to get moving! Do {reps} reps of {exercise}. No excuses, champ!")
+    print(f"Time to get moving! Do {reps} reps of {exercise}.")
 
 if __name__ == "__main__":
     while True:
         virtual_workout()
-        # Wait at least 30 minutes (randomized: between 30 and 45 minutes)
-        wait_time = 30 * 60 + random.randint(0, 15 * 60)
+        # Wait 30 minutes
+        wait_time = 30 * 60
         minutes = wait_time // 60
         print(f"Rest up, you'll get another surprise workout in about {minutes} minutes.")
         time.sleep(wait_time)
